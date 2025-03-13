7b5a29dd1b4934cff0ba496fca702dba5fd00c01
mehedi1256 <mehedihassanshovospirit@gmail.com>
2025-03-13 15:29:13 +0600

    login page customizer plugin

---
+++ login-page-customizer.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Login Page Customizer
+ * Description: Customize the login page
+ * Author: Mehedi Hassan Shovo
+ * Version: 1.0
+ * Author URI: http://example.com  
+ * Plugin URI: http://example.com
+ * Text Domain: login-page-customizer
+ */
